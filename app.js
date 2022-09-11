b7437a08b159adb885ffb754098ed85efaa740d0
ipeerless <ipeerless0@gmail.com>
2022-09-11 14:41:16 +0200

    some work

--- app.js
+++ app.js
@@ -2,41 +2,35 @@ const app = Vue.createApp({
 
 data () {
 return {
+    url: 'http://www.thenetninja.co.uk',
 showBooks: true,
-title: 'the final work',
-author: 'Adam',
-age: "46",
-x: 0,
-y: 0
-
-
+books: [
+    {title: 'my life', author: 'me', img:'assets/1.jpg', isFav: true},
+    {title: 'his life', author: 'him', img:'assets/2.jpg', isFav: false},
+    {title: 'your life', author: 'you', img:'assets/3.jpg', isFav: true},
+]
 }
 },
 methods: {
-// changeTitle (title) {
 
-// this.title = title
 
-// },
+
 toggleShowBooks() {
 this.showBooks = !this.showBooks
 
 },
-handleEvent (e, data) {
-console.log(e, e.type)
-if (data) {
-console.log(data)
+toggleFav(book) {
+    book.isFav = !book.isFav
+    
+    }
+    },
+    computed: {
+        filteredBooks () {
 
-}
-
-},
-handleMousemove(e) {
-    this.x = e.offsetX
-    this.y = e.offsetY
-}
+            return this.books.filter((book) => book.isFav)
+        }
 
 
-
-} 
+    }
 })
 app.mount('#app')
